de44a48a8d0741bc713bfb58b8484e5fd05809a0
Kravmi <144707588+Kravmi@users.noreply.github.com>
2023-11-10 17:52:40 +0300

    Сделали врагов, а также начали создание меню.

--- shooter_game.py
+++ shooter_game.py
@@ -1,6 +1,8 @@
 # Создай собственный Шутер!
 import pygame as pg
 import constants as cnst
+import random
+import pygame_menu as menu
 
 
 class GameSprite(pg.sprite.Sprite):
@@ -27,11 +29,28 @@ class Player(GameSprite):
     def fire(self):
         pass
 
+class Enemy(GameSprite):
+    def update(self):
+        if self.rect.y < cnst.WIDTH_WINDOW:
+            self.rect.y += self.speed
+        else: 
+            global enemy_finish 
+            enemy_finish += 1
+            self.rect.y = 0
+            self.rect.x = random.randint(30, cnst.HEIGHT_WINDOW - 30)
 
+def start_game():
+    global main_menu
+    main_menu.disable()
+    main_menu.full_reset()
+
+
+pg.init()
 window = pg.display.set_mode((cnst.WIDTH_WINDOW, cnst.HEIGHT_WINDOW))
 pg.display.set_caption("Space game")
 game = True
 finish = False
+enemy_finish = 0
 clock = pg.time.Clock()
 background = pg.transform.scale(pg.image.load('galaxy.jpg'), \
     (cnst.WIDTH_WINDOW, cnst.HEIGHT_WINDOW))
@@ -40,13 +59,34 @@ pg.mixer.music.load('space.ogg')
 pg.mixer.music.set_volume(0.1)
 pg.mixer.music.play()
 player = Player('rocket.png', 80, 100, 5, 400, 10)
+monsters = pg.sprite.Group()
+main_menu = menu.Menu('Space Game', cnst.WIDTH_WINDOW, cnst.HEIGHT_WINDOW)
+main_menu.add.button('PLAY', start_game)
+main_menu.add.button('EXIT', menu.events.EXIT)
+pg.font.init()
+font = pg.font.Font(None, 36)
+for i in range(5):
+    monster = Enemy('ufo.png', 80, 50, random.randint(30, cnst.HEIGHT_WINDOW - 30), random.randint(20, 30), random.randint(1, 3))
+    monsters.add(monster)
+    
 while game:
     for i in pg.event.get():
         if i.type == pg.QUIT:
             game = False
+    if main_menu.is_enabled():
+        main_menu.mainloop(window)
     if not finish:
         window.blit(background, (0, 0))
         player.reset()
         player.update()
+        monsters.update()
+        monsters.draw(window)
+        count = font.render('Счет:', 1, (255, 255, 255))
+        window.blit(count, (10, 20))
+        lose = font.render(f'Пропущено: {enemy_finish}', 1, (255, 255, 255))
+        window.blit(lose, (10, 50))
+        if enemy_finish >= 3:
+            finish = True
+            main_menu.enable()
     pg.display.update()
     clock.tick(cnst.FPS)
